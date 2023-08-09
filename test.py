e2a43c6503c005ffed40edf5c78e1101eed12fb0
nahsc0 <naseeryusuf07@gmail.com>
2023-08-09 15:10:37 +0100

    added a GUI capability to the application

--- test.py
+++ test.py
@@ -1,8 +1,27 @@
+import tkinter as tk
+
 def greet_instructor():
     name = "Mr. Adefemi Michel"
     greeting = f"Hello, {name}!"
-    message = "We are gratefully for having you as our instructor."
+    message = "We are grateful for having you as our instructor."
     message += " Your vast wisdom as a DevOps Engineer is truly inspiring."
     return greeting + " " + message
 
-print(greet_instructor())
+def show_greeting():
+    # This function will be called when the button is clicked.
+    greeting = greet_instructor()
+    label.config(text=greeting)
+
+# Create a new window
+root = tk.Tk()
+
+# Create a label to display the greeting
+label = tk.Label(root, text="")
+label.pack()
+
+# Create a button that will trigger the greeting
+button = tk.Button(root, text="Greet Instructor", command=show_greeting)
+button.pack()
+
+# Run the application
+root.mainloop()
